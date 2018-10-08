4cf54eec5860d0d347c555dc709fe9819729e0b0
Michael Nield <michael.nield@screenmedia.co.uk>
2018-10-08 09:56:19 +0100

    Console errors fixed

--- src/Components/Repo.js
+++ src/Components/Repo.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Repo = props => (
   <li> 
     <div className="left">
-      <img src={props.img} />
+      <img src={props.img} alt="profile"/>
     </div>
     <div className="right">    
-      <a href={props.url} target="_BLANK">{props.name}</a>
+      <a href={props.url} target="_blank" rel="noopener noreferrer">{props.name}</a>
       <span>
         <i className="material-icons">call_split</i>
         {props.forks}
